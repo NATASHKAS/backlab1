3338bd38795b882081a8fc6c2e9b5b51c8d0e65f
Natalia <nyancatandme0@gmail.com>
2022-10-30 23:16:41 +0200

    more endpoints

--- module/views.py
+++ module/views.py
@@ -8,12 +8,77 @@ CATEGORIES = [
     }
 ]
 
+USERS = [
+    {
+        "id": 1,
+        "name": "Natashka"
+    }
+]
+
+NOTES = [
+    {
+        "id": 1,
+        "user_id": 1,
+        "category_id": 1,
+        "time": "30/10/2022",
+        "expenses": 1000
+    }
+]
+
+@app.route("/users")
+def get_users():
+    return jsonify({"users": USERS})
+
 @app.route("/categories")
 def get_categories():
-    return jsonify({"categiries": CATEGORIES})
+    return jsonify({"categories": CATEGORIES})
 
-@app.route("/category", methods=["POST"])
+@app.route("/notes")
+def get_notes():
+    return jsonify({"notes": NOTES})
+
+@app.route("/usernotes")
+def get_usernotes():
+    id = request.args.get("userid")
+    notes = []
+    for note in NOTES:
+        if note["user_id"] == int(id):
+            notes.append(note)
+    return jsonify({"notes": notes})
+
+@app.route("/categorynotes")
+def get_categorynotes():
+    category_id = request.args.get("categoryid")
+    user_id = request.args.get("userid")
+    notes = []
+    for note in NOTES:
+        if note["category_id"] == int(category_id) and note["user_id"] == int(user_id):
+            notes.append(note)
+    return jsonify({"notes": notes})
+
+@app.route("/adduser", methods=["POST"])
+def create_user():
+    request_data = request.get_json()
+    for user in USERS:
+        if (user["id"] == request_data["id"]):
+            return "Please, enter another id"
+    USERS.append(request_data)
+    return request_data
+
+@app.route("/addcategory", methods=["POST"])
 def create_category():
     request_data = request.get_json()
+    for category in CATEGORIES:
+        if (category["id"] == request_data["id"]):
+            return "Please, enter another id"
     CATEGORIES.append(request_data)
+    return request_data
+
+@app.route("/addnote", methods=["POST"])
+def create_note():
+    request_data = request.get_json()
+    for note in NOTES:
+        if (note["id"] == request_data["id"]):
+            return "Please, enter another id"
+    NOTES.append(request_data)
     return request_data
