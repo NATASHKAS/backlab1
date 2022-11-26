20511a20f6f84cdb03d70503871539c0523a08a8
Natalia <nyancatandme0@gmail.com>
2022-11-26 16:58:56 +0200

    install flask-smorest and move USERS,CATEGORIES,NOTES to db.py

---
+++ module/db.py
@@ -0,0 +1,23 @@
+CATEGORIES = [
+    {
+        "id": 1,
+        "name": "food"
+    }
+]
+
+USERS = [
+    {
+        "id": 1,
+        "name": "Natashka"
+    }
+]
+
+NOTES = [
+    {
+        "id": 1,
+        "user_id": 1,
+        "category_id": 1,
+        "time": "30/10/2022",
+        "expenses": 1000
+    }
+]
--- module/views.py
+++ module/views.py
@@ -1,30 +1,7 @@
 from flask import jsonify, request
 from module import app
 
-CATEGORIES = [
-    {
-        "id": 1,
-        "name": "food"
-    }
-]
-
-USERS = [
-    {
-        "id": 1,
-        "name": "Natashka"
-    }
-]
-
-NOTES = [
-    {
-        "id": 1,
-        "user_id": 1,
-        "category_id": 1,
-        "time": "30/10/2022",
-        "expenses": 1000
-    }
-]
-
+from module.db import USERS,CATEGORIES,NOTES
 @app.route("/users")
 def get_users():
     return jsonify({"users": USERS})
